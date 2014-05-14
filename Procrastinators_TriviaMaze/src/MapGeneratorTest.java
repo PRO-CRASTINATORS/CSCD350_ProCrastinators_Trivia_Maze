a52440f82259ca7f10462c56442990d7789f2b99
kevinMurr <kmurray@eagle.ewu.edu>
2014-05-13 23:01:34 -0700

    Maze generator

---
+++ Procrastinators_TriviaMaze/src/MapGeneratorTest.java
@@ -0,0 +1,10 @@
+
+public class MapGeneratorTest 
+{
+	public static void main(String[] args)
+	{
+		String[][] protoMap = MazeGenerator.loadMap("C:/Users/Kevin/Documents/GitHub/CSCD350_ProCrastinators_Trivia_Maze/Procrastinators_TriviaMaze/src/map1.txt");
+		
+		MazeGenerator.printArray(protoMap);
+	}
+}
