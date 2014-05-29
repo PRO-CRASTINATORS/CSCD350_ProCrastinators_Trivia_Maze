bf691f0b813054fb4ef7a6718a75cc43254b5fcd
ewu-phillipsk <kphillips2010@eagles.ewu.edu>
2014-05-28 18:48:02 -0700

    Updated Database (Java)
    Fixed bugs, made the database an object now rather than using static
    methods. Test included.

--- DatabaseStuff/DatabaseProject/DatabaseProject/DatabaseHandler.java
+++ DatabaseStuff/DatabaseProject/DatabaseProject/DatabaseHandler.java
@@ -1,164 +1,68 @@
 import java.sql.*;
 public class DatabaseHandler
 {
-	public static ResultSet getTriviaByCategory(String s)
+	private Connection cn = null;
+	private Statement stmt = null;
+	
+	public DatabaseHandler()
 	{
-		Connection cn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
 			cn = DriverManager.getConnection("jdbc:sqlite:TriviaItems.db");
 			cn.setAutoCommit(false);
-			System.out.println("Opened database successfully");
-			
 			stmt = cn.createStatement();
-			rs = stmt.executeQuery( "SELECT * FROM trivia_item WHERE category = '" + s + "';" );
-			
-			while(rs.next())
-			{
-				String question = rs.getString("question");
-				String answer = rs.getString("answer");
-				String a = rs.getString("a");
-				String b = rs.getString("b");
-				String c = rs.getString("c");
-				String d = rs.getString("d");
-				int type = rs.getInt("type");
-				String category = rs.getString("category");
-
-				System.out.println("Question = " + question);
-				System.out.println("Answer = " + answer);
-				System.out.println("A = " + a);
-				System.out.println("B = " + b);
-				System.out.println("C = " + c);
-				System.out.println("D = " + d);
-				System.out.println("Type = " + type);
-				System.out.println("Category = " + category);
-			}//end while
-			stmt.close();
-			cn.close();
-			System.out.println("Closed database successfully");
-		}//end try
-		catch ( Exception e ) 
-		{
-		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		      System.exit(0);
-		}//end catch
-		System.out.println("Operation done successfully!");
-		return rs;
-	}//end getTriviaByCategory
-	
-	
-	
-	public static ResultSet getAllTrivia()
-	{
-		Connection cn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try
-		{
-			Class.forName("org.sqlite.JDBC");
-			cn = DriverManager.getConnection("jdbc:sqlite:TriviaItems.db");
-			cn.setAutoCommit(false);
 			System.out.println("Opened database successfully");
-			
-			stmt = cn.createStatement();
-			rs = stmt.executeQuery( "SELECT * FROM trivia_item" );
-			
-			while(rs.next())
-			{
-				String question = rs.getString("question");
-				String answer = rs.getString("answer");
-				String a = rs.getString("a");
-				String b = rs.getString("b");
-				String c = rs.getString("c");
-				String d = rs.getString("d");
-				int type = rs.getInt("type");
-				String category = rs.getString("category");
-
-				System.out.println("Question = " + question);
-				System.out.println("Answer = " + answer);
-				System.out.println("A = " + a);
-				System.out.println("B = " + b);
-				System.out.println("C = " + c);
-				System.out.println("D = " + d);
-				System.out.println("Type = " + type);
-				System.out.println("Category = " + category);
-			}//end while
-			stmt.close();
-			cn.close();
-			System.out.println("Closed database successfully");
-		}//end try
-		catch ( Exception e ) 
-		{
-		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		      System.exit(0);
-		}//end catch
-		System.out.println("Operation done successfully!");
-		return rs;
-	}//end getAllTrivia
-	
-	
-	
-	public static ResultSet getCategories()
-	{
-		Connection cn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try
-		{
-			Class.forName("org.sqlite.JDBC");
-			cn = DriverManager.getConnection("jdbc:sqlite:TriviaItems.db");
-			cn.setAutoCommit(false);
-			System.out.println("Opened database successfully");
-			
-			stmt = cn.createStatement();
-			rs = stmt.executeQuery( "SELECT DISTINCT category FROM trivia_item;" );
-		
-			while(rs.next())
-			{
-				String category = rs.getString("category");
-				System.out.println("Category = " + category);
-			}//end while
-			stmt.close();
-			cn.close();
-			System.out.println("Closed database successfully");
-		}//end try
-		catch ( Exception e ) 
-		{
-		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-		      System.exit(0);
-		}//end catch
-		System.out.println("Operation done successfully!");
-		return rs;
-	}//end getCategories
-	
-	
-	
-	public static void printCategories(ResultSet rs)
-	{
-		try
-		{
-			while(rs.next())
-			{
-				String category = rs.getString("category");
-				System.out.println("Category = " + category);
-			}//end while
 		}
-		catch(Exception e)
+		catch ( Exception e ) 
 		{
-			System.out.println("Error printing result set");
+		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		      System.exit(0);
+		}//end catch
+	}//end DatabaseHandler constructor
+	
+	
+	
+	public ResultSet getTriviaByCategory(String s) throws SQLException
+	{
+		return stmt.executeQuery( "SELECT * FROM trivia_item WHERE category = '" + s + "';" );
+	}
+	
+	
+	
+	public ResultSet getAllTrivia() throws SQLException
+	{
+		return stmt.executeQuery( "SELECT * FROM trivia_item" );
+	}
+	
+	
+	
+	public ResultSet getCategories() throws SQLException
+	{
+		return stmt.executeQuery( "SELECT DISTINCT category FROM trivia_item;" );
+	}
+	
+	public void printCategories(ResultSet rs)
+	{		
+		try
+		{
+			while(rs.next())
+			{
+				String category = rs.getString("category");
+				System.out.println("Category = " + category);
+			}//end while
+			rs.close();
 		}
-		
-	}//end getCategories
+		catch ( Exception e ) 
+		{
+		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		      System.exit(0);
+		}//end catch
+	}//end printCategories
 	
 	
 	
-	public static void printTrivia(ResultSet rs)
+	public void printTrivia(ResultSet rs)
 	{
 		try
 		{
@@ -184,9 +88,19 @@ public class DatabaseHandler
 			}//end while
 			rs.close();
 		}
-		catch(Exception e)
+		catch ( Exception e ) 
 		{
-			System.out.println("Error printing Trivia");
-		}
+		      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+		      System.exit(0);
+		}//end catch
 	}//end getCategories
+	
+	
+	
+	public void closeDatabase() throws SQLException
+	{
+		this.stmt.close();
+		this.cn.close();
+		System.out.println("Closed database successfully");
+	}
 }//end class
--- DatabaseStuff/DatabaseProject/DatabaseProject/DatabaseHandlerTester.java
+++ DatabaseStuff/DatabaseProject/DatabaseProject/DatabaseHandlerTester.java
@@ -2,20 +2,21 @@ import java.sql.*;
 
 public class DatabaseHandlerTester {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws SQLException 
 	{
 		ResultSet rsTester = null;
+		DatabaseHandler db = new DatabaseHandler();
 		
-		rsTester = DatabaseHandler.getCategories();
-		DatabaseHandler.printCategories(rsTester);
+		rsTester = db.getCategories();
+		db.printCategories(rsTester);
+
+		rsTester = db.getTriviaByCategory("Mario");
+		db.printTrivia(rsTester);
 		
-		rsTester = DatabaseHandler.getTriviaByCategory("Zelda");
-		DatabaseHandler.printTrivia(rsTester);
+		rsTester = db.getAllTrivia();
+		db.printTrivia(rsTester);
 		
-		rsTester = DatabaseHandler.getAllTrivia();
-		DatabaseHandler.printTrivia(rsTester);
-		
+		db.closeDatabase();
 		System.out.println("Tests complete");
 	}
-
 }
